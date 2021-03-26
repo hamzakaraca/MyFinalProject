d3138e3505e28d3217dce5d4135d7e399072e883
hamzakaraca <79093890+hamzakaraca@users.noreply.github.com>
2021-03-26 11:02:46 +0300

    hafta 8 9
    beraber

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categorDal;
+
+        public CategoryManager(ICategoryDal categorDal)
+        {
+            _categorDal = categorDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categorDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categorDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
